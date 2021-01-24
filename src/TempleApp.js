4bd7f33f537f29ad71b481cae07b21accd05cc3e
gricardov <gricardov@gmail.com>
2021-01-23 22:32:19 -0500

    fix

--- src/TempleApp.js
+++ src/TempleApp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Inicio from './pages/inicio';
 import Teachers from './pages/teachers';
 import TeacherDetail from './pages/teacher_detail';
@@ -16,25 +16,22 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 const TempleApp = () => {
 
     return (
-        <AuthProvider>
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Inicio} />
-                    <Route path="/courses" component={Courses} />
-                    <Route path="/teachers" component={Teachers} />
-                    <Route path="/teacher-detail" component={TeacherDetail} />
-                    <Route path="/course" component={Course} />
-                    <Route path="/course-player" component={CoursePlayer} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/about" component={About} />
-                    <Route path="/login" render={() => {
 
-                    }} component={Login} />
-                    <PrivateRoute exact path="/admin" component={Admin} />
-                    <Redirect to="/" />
-                </Switch>
-            </Router>
-        </AuthProvider>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Inicio} />
+                <Route path="/courses" component={Courses} />
+                <Route path="/teachers" component={Teachers} />
+                <Route path="/teacher-detail" component={TeacherDetail} />
+                <Route path="/course" component={Course} />
+                <Route path="/course-player" component={CoursePlayer} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/about" component={About} />
+                <Route path="/login" component={Login} />
+                <PrivateRoute exact path="/admin" component={Admin} />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
     )
 }
 
