f3e1498959402bc57ff8d814e4ee5ba0d2c7e6ec
gricardov <gricardov@gmail.com>
2021-01-26 00:48:46 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TempleApp from './TempleApp';
+import "bootstrap/dist/js/bootstrap.min.js";
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles/normalize.css';
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
-import "bootstrap/dist/js/bootstrap.min.js";
-import 'bootstrap/dist/css/bootstrap.css';
 import "@fortawesome/fontawesome-free/css/all.css";
 import "plyr/dist/plyr.css";
+import './styles/index.css';
 
 ReactDOM.render(
   <TempleApp />,
