f5baf160af3a174f90a1eabe215892244b57175b
gricardov <gricardov@gmail.com>
2021-01-26 01:35:35 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TempleApp from './TempleApp';
-import "bootstrap/dist/js/bootstrap.min.js";
-import 'bootstrap/dist/css/bootstrap.css';
+//import "bootstrap/dist/js/bootstrap.min.js";
+//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/normalize.css';
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
