0e5a238b3ec319d8b4a0ad8774b3a3f334ac66cb
gricardov <gricardov@gmail.com>
2021-04-21 16:18:12 -0500

    fixed certs

--- src/helpers/funcionesTexto.js
+++ src/helpers/funcionesTexto.js
@@ -1,8 +1,25 @@
+import moment from 'moment';
+import "moment/locale/es";
+
 export const obtenerOracion = (texto, limite) => {
-    limite = !limite ? texto.length : limite;
-    if (texto && texto.length > 0) {
-      return texto.substring(0, 1).toUpperCase() + texto.substring(1, limite);
-    } else {
-      return '';
-    }
+  limite = !limite ? texto.length : limite;
+  if (texto && texto.length > 0) {
+    return texto.substring(0, 1).toUpperCase() + texto.substring(1, limite);
+  } else {
+    return '';
   }
+}
+
+export const aOracion = (text, limit) => {
+  limit = !limit ? text.length : limit;
+  if (text && text.length > 0) {
+    return text.substring(0, 1).toUpperCase() + text.substring(1, limit);
+  } else {
+    return '';
+  }
+}
+
+export const obtenerFechaTexto = (date) => {
+  const momentObj = moment(date);
+  return aOracion(momentObj.format('D [de] MMMM [del] YYYY'));
+}
--- src/pages/view_cert.js
+++ src/pages/view_cert.js
@@ -3,14 +3,16 @@ import Navbar from '../components/navbar';
 import Footer from "../components/footer";
 import GridLoader from "react-spinners/GridLoader";
 import queryString from 'query-string';
+import Avatar from "../components/avatar"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { FacebookShareButton } from "react-share";
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { obtenerFechaTexto } from '../helpers/funcionesTexto';
 import { obtCertificado } from '../api';
 import { css } from "@emotion/core";
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const override = css`
   display: block;
@@ -25,6 +27,8 @@ const Previsualizacion = ({ location }) => {
     const [origin, setOrigin] = useState('OTHER');
     const [course, setCourse] = useState('');
     const [name, setName] = useState('');
+    const [teachers, setTeachers] = useState([]);
+    const [issuedAt, setIssuedAt] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     const [resultUrl, setResultUrl] = useState('');
     const [success, setSuccess] = useState(false);
@@ -56,11 +60,13 @@ const Previsualizacion = ({ location }) => {
         if (idCert) {
             obtCertificado(idCert).then(({ data, error }) => {
                 if (!error) {
-                    const { resultUrl, course, teachers, issuedAt, name } = data;
+                    const { urlCert, curso, profesores, creadoEn, otorgadoA } = data;
                     setId(idCert);
-                    setResultUrl(resultUrl);
-                    setCourse(course);
-                    setName(name);
+                    setResultUrl(urlCert);
+                    setCourse(curso);
+                    setTeachers(profesores);
+                    setIssuedAt(creadoEn);
+                    setName(otorgadoA);
                     setIsLoading(false);
                 } else {
                     alert('No se encontró el archivo. Intente más tarde');
@@ -90,17 +96,16 @@ const Previsualizacion = ({ location }) => {
     const url = process.env.REACT_APP_WEBSITE + '/' + window.location.pathname;
 
     return (
-
         <div>
             <Navbar />
             <div className="cuerpo-pagina">
                 <GridLoader css={override} loading={isLoading} size={20} />
-                <section className='contenedor-visor-pdf position-relative'>
-                    <div>
-                        {
-                            resultUrl && !isLoading &&
-                            <>
-                                <h2 className="titulo-seccion mb-4">{course.name}</h2>
+                {
+                    resultUrl && !isLoading &&
+                    <>
+                        <section className='contenedor-visor-pdf position-relative'>
+                            <div>
+                                <h2 className="titulo-seccion mb-5">{course.nombre}</h2>
                                 <Document
                                     file={resultUrl}
                                     onLoadSuccess={onDocumentLoadSuccess}
@@ -118,10 +123,48 @@ const Previsualizacion = ({ location }) => {
                                         )
                                     }
                                 </Document>
-                            </>
-                        }
-                    </div>
-                </section>
+                            </div>
+                        </section>
+                        <section className='contenedor contenedor-60 contenedor-datos-certificado'>
+                            <h3 className="subtitulo-descripcion-curso mt-4">Otorgado a:</h3>
+                            <p className="descripcion-curso">
+                                {name}
+                            </p>
+                            <h3 className="subtitulo-descripcion-curso">Condición lograda:</h3>
+                            <p className="descripcion-curso">
+                                Completar el taller en vivo "Introducción a Spring Boot y Despliegue Continuo"
+                    </p>
+                            <h3 className="subtitulo-descripcion-curso">Profesores:</h3>
+                            {
+                                teachers.map(profesor => (
+                                    <div className="contenedor-curso-profesor">
+                                        <div className="contenedor-img">
+                                            <Avatar img={profesor.imgUrl} />
+                                        </div>
+                                        <div className="contenedor-descripcion">
+                                            <h4>
+                                                <Link to={`/teacher-detail/?id=${profesor.id}`}>
+                                                    {profesor.nombres}
+                                                </Link>
+                                            </h4>
+                                            <p className="descripcion-curso clamp clamp-2">
+                                                {profesor.sobreMi}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                            <h3 className="subtitulo-descripcion-curso">Curso:</h3>
+                            <p className="descripcion-curso">
+                                <a href="_blank" href={course.url}>{course.nombre}</a>
+                            </p>
+                            <h3 className="subtitulo-descripcion-curso">Emitido el:</h3>
+                            <p className="descripcion-curso">
+                                {obtenerFechaTexto(issuedAt.seconds * 1000)}
+                            </p>
+                        </section>
+                    </>
+                }
             </div>
             <Footer />
             <div className='bottom-prev-navbar'>
