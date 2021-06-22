8ec9d065905286808b2466cb9c36abdaf2ae81ff
gricardov <gricardov@gmail.com>
2021-06-21 23:58:39 -0500

    form cpp pucp eliminado

--- src/pages/enr_event.js
+++ src/pages/enr_event.js
@@ -4,8 +4,8 @@ import APRENDE_CPP_LOPEZ_1 from './enr_event_APRENDE-CPP-LOPEZ-1';
 const Inscripcion = ({ match }) => {
 
     switch (match.params.id) {
-        case 'APRENDE-CPP-LOPEZ-1':
-            return <APRENDE_CPP_LOPEZ_1 />;
+        //case 'APRENDE-CPP-LOPEZ-1':
+        //return <APRENDE_CPP_LOPEZ_1 />;
         default:
             return <div>Código de inscripción inválido</div>;
     }
