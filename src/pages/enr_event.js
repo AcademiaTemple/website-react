e9e508d20af4f339d2c7b16fd01d1bf334b2e92f
axel <axel.chacon@pucp.edu.pe>
2021-09-04 13:47:38 -0500

    formularios de prueba

--- src/pages/enr_event.js
+++ src/pages/enr_event.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import FormCPPLopez from './enr_events/APRENDE-CPP-LOPEZ-1';
 import FormIngles from './enr_events/APRENDE-INGLES-BENAVIDES-1';
-
+import FormMongoDB from './enr_events/APRENDE-MONGODB-FATAMA-1';
 const Inscripcion = ({ match }) => {
 
     switch (match.params.id) {
@@ -9,6 +9,9 @@ const Inscripcion = ({ match }) => {
             return <FormCPPLopez />;
         case 'APRENDE-INGLES-BENAVIDES-1':
             return <FormIngles />;
+        case 'APRENDE-MONGODB-FATAMA-1':
+            return <FormMongoDB/>;
+
         default:
             return <div>Código de inscripción inválido</div>;
     }
