4fdb8556ff9821a15869138cf4b625e2227a699c
gricardov <gricardov@gmail.com>
2021-09-08 17:10:59 -0500

    test

--- src/pages/enr_event.js
+++ src/pages/enr_event.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FormCPPLopez from './enr_events/APRENDE-CPP-LOPEZ-1';
 import FormIngles from './enr_events/APRENDE-INGLES-BENAVIDES-1';
+import FormMongo from './enr_events/APRENDE-MONGO-FATAMA-1';
 
 const Inscripcion = ({ match }) => {
 
@@ -9,6 +10,8 @@ const Inscripcion = ({ match }) => {
             return <FormCPPLopez />;
         case 'APRENDE-INGLES-BENAVIDES-1':
             return <FormIngles />;
+        case 'APRENDE-MONGO-FATAMA-1':
+            return <FormMongo />;
         default:
             return <div>Código de inscripción inválido</div>;
     }
